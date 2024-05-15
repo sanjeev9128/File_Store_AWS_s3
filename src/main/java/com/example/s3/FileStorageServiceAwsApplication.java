964be82ef5919f6834de5f1957684ce051330121
sanjeev kumar gond <sskumar9128@gmail.com>
2024-05-15 10:36:37 +0530

    Add file without aws s3 connection

---
+++ src/main/java/com/example/s3/FileStorageServiceAwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageServiceAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageServiceAwsApplication.class, args);
+	}
+
+}
